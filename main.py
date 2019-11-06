65e191639dd2d75c69c587b94906df14c332abf3
NishiumiRyutaro <56168742+NishiumiRyutaro@users.noreply.github.com>
2019-11-06 17:45:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 import discord
+import re
 
 token = os.getenv('DISCORD_TOKEN')
 
@@ -14,9 +15,18 @@ async def on_ready():
 async def on_message(message):
     if message.author.bot:
         return
-    if message.content == '/hello':
-        await message.channel.send('はろー')
-    elif message.content == '/evening':
-        await message.channel.send('こんばんは')
+    if message.content != None:
+        x = message.content
+        m = re.search(r'つよばは21時', x, flags=re.DOTALL)
+        if m :
+            await message.channel.send('21時からつよばは連戦部屋建て&環境役をやります。参加予定の方は30分前までにスプシに自発予定とあわせて書きこんでおいてください。人数によっては部屋を分ける場合もあります。')
+    await message.delete()            
+    await message.channel.send('削除しました')
+        #y = m.group(0) if m else None
+        #await message.channel.send(repr(y))
+    #if message.content == '/hello':
+    #    await message.channel.send('はろー')
+    #elif message.content == '/evening':
+    #    await message.channel.send('こんばんは')
 
-client.run(token)
+client.run("token")
