d13001e48b5371e8728411418aa914b5f7ff225b
NishiumiRyutaro <56168742+NishiumiRyutaro@users.noreply.github.com>
2019-11-14 00:54:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import discord
 from datetime import datetime
 from discord.ext import tasks
 import re
+import asyncio
 
 token = os.getenv('DISCORD_TOKEN')
 channel_id = 629674683695890449
@@ -12,19 +13,18 @@ client = discord.Client()
 
 
 #投稿する日時
-DefensedateTimeList = [
-'09:50',
-'09:51',
-'09:52',
-'09:53',
-'09:54',
-'09:55',
-'15:14'
+AlldateTimeList = [
+ '11:00:00'
+]
+ADdateTimeList = [
+ '12:00:00' 
 ]
 FOdateTimeList = [
-'15:14',
-'07:00'
-]
+ '14:00:00'
+ ]
+readydateTimeList = [
+ '22:00:00'
+ ]
 ProtCommndList = [
     "20時30分からつよばは連戦部屋建て&環境役をやります。参加予定の方は30分前までにスプシに自発予定とあわせて書きこんでおいてください。人数によっては部屋を分ける場合もあります。",
     "21時からつよばは連戦部屋建て&環境役をやります。参加予定の方は30分前までにスプシに自発予定とあわせて書きこんでおいてください。人数によっては部屋を分ける場合もあります。",
@@ -36,8 +36,14 @@ ProtCommndList = [
 @client.event
 async def on_ready():
     print('ready')
-    
-async def DSendMessage():
+
+async def AllSendMessage():
+    channel = client.get_channel(channel_id)
+    await channel.send('攻撃アビの時間だよ')
+    await channel.send('防衛アビの時間だよ')
+    await channel.send('副団アビの時間だよ')
+    await asyncio.sleep(35)    
+async def ADSendMessage():
     channel = client.get_channel(channel_id)
     await channel.send('攻撃アビの時間だよ')
     await channel.send('防衛アビの時間だよ')
@@ -46,16 +52,23 @@ async def FOSendMessage():
     channel = client.get_channel(channel_id)
     await channel.send('副団アビの時間だよ')
     await asyncio.sleep(35)
+async def readySendMessage():
+    channel = client.get_channel(channel_id)
+    await channel.send('明日は古戦場だよ')
+    await asyncio.sleep(35)
 # 30秒に一回ループ
 @tasks.loop(seconds=0)
 async def time_check():
-    sleepTime = 0
     # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
-    if now in DefensedateTimeList:
-        await DSendMessage()
+    now = datetime.now().strftime('%H:%M:%S')
+    if now in ADdateTimeList:
+        await ADSendMessage()
+    if now in AlldateTimeList:
+        await AllSendMessage()
     if now in FOdateTimeList:
-        await FOSendMessage()    
+        await FOSendMessage()
+    if now in readydateTimeList:
+        await readySendMessage()   
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
@@ -64,7 +77,7 @@ async def on_message(message):
         return
     # 使用できるコマンド一覧
     if message.content == '!nowtime':
-        now = datetime.now().strftime('%H:%M')
+        now = datetime.now().strftime('%H:%M:%S')
         await message.channel.send(now)
     if message.content != None:
         x = message.content
