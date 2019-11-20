fa461435673a37c5792032be8ab879c21a21bda2
NishiumiRyutaro <56168742+NishiumiRyutaro@users.noreply.github.com>
2019-11-21 01:17:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -47,6 +47,8 @@ async def on_message(message):
     if message.content == '!nowtime':
         now = datetime.now().strftime('%H:%M:%S')
         await message.channel.send(now)
+    elif message.content == '!specification':
+     　 await message.channel.send('spバトル仕様だよ')
     if message.content != None:
         x = message.content
         if x.find('!') == 0:
