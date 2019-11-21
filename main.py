8baff5f8a90e387a77ec8a1f12d590a7473ef6f3
NishiumiRyutaro <56168742+NishiumiRyutaro@users.noreply.github.com>
2019-11-21 17:38:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import asyncio
 from foo import ProtCommndList
 from foo import AruCommndList
 from foo import RushiCommndList
+from foo import allcmdlist
+from foo import MarisuCommndList
 
 token = os.getenv('DISCORD_TOKEN')
 channel_id = 644583189331050537
@@ -29,37 +31,121 @@ async def time_check():
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
-        return
-    # 使用できるコマンド一覧
-    if message.content == '!specification':
-        await message.channel.send('sp')
-    elif message.content == '!nowtime':
-        now = datetime.now().strftime('%H:%M:%S')
-        await message.channel.send(now)
+        return    
+    if message.content == 'list':
+        testcmd = ",".join(allcmdlist)                              # コマンドリスト関連
+        testcmd_new = testcmd.replace(',', '\n')                    #
+        allcmdlist_new =  testcmd_new.split(",")                    #
+        await message.channel.send(testcmd_new.replace('"', '\n'))  #
     if message.content != None:
         x = message.content
-        if x.find('!') == 0:
-            await message.delete()
-        m = re.search(r'!つよばは20時30分', x, flags=re.DOTALL)
-        l = re.search(r'!つよばは21時', x, flags=re.DOTALL)
-        o = re.search(r'!つよばは21時30分', x, flags=re.DOTALL)
-        p = re.search(r'!つよばは22時', x, flags=re.DOTALL)
-        q = re.search(r'!つよばは22時30分', x, flags=re.DOTALL)
-        arubaha = re.search(r'!あるばは21時', x, flags=re.DOTALL)
-        if m :
-            await message.channel.send(ProtCommndList[0])
-        if l :
-            await message.channel.send(ProtCommndList[1])
-        if o :
-            await message.channel.send(ProtCommndList[2])
-        if p :
-            await message.channel.send(ProtCommndList[3])
-        if q :
-            await message.channel.send(ProtCommndList[4])        
-        if arubaha :
-            await message.channel.send(AruCommndList[0])
+        a = re.search(r',.*', x, flags=re.DOTALL)
+        #b = re.search(r'@.*', x, flags=re.DOTALL)
+        if re.findall('^!', x, flags=re.IGNORECASE):
+            tm = re.search(r'つよばは2030', x, flags=re.DOTALL)
+            tl = re.search(r'つよばは21', x, flags=re.DOTALL)
+            tn = re.search(r'つよばは2130', x, flags=re.DOTALL)
+            to = re.search(r'つよばは22', x, flags=re.DOTALL)
+            tp = re.search(r'つよばは2230', x, flags=re.DOTALL)
+            mm = re.search(r'まりす2030', x, flags=re.DOTALL)
+            ml = re.search(r'まりす21', x, flags=re.DOTALL)
+            mn = re.search(r'まりす2130', x, flags=re.DOTALL)
+            mo = re.search(r'まりす22', x, flags=re.DOTALL)
+            mp = re.search(r'まりす2230', x, flags=re.DOTALL)
+            am = re.search(r'!あるばは2030', x, flags=re.DOTALL)
+            al = re.search(r'!あるばは21', x, flags=re.DOTALL)
+            an = re.search(r'!あるばは2130', x, flags=re.DOTALL)
+            ao = re.search(r'!あるばは22', x, flags=re.DOTALL)
+            ap = re.search(r'!あるばは2230', x, flags=re.DOTALL)
+            rm = re.search(r'るし2100', x, flags=re.DOTALL)
+            rl = re.search(r'るし2130', x, flags=re.DOTALL)
+            rn = re.search(r'るし2200', x, flags=re.DOTALL)
+            ro = re.search(r'るし2230', x, flags=re.DOTALL)
+            rp = re.search(r'るし2300', x, flags=re.DOTALL)           
+        if a != None :            
+            a = a.group(0).replace(',', '')             
+            if tm:
+                await message.channel.send(a+'日'+ProtCommndList[0])
+            if tl:
+                await message.channel.send(a+'日'+ProtCommndList[1])
+            if tn:
+                await message.channel.send(a+'日'+ProtCommndList[2])
+            if to:
+                await message.channel.send(a+'日'+ProtCommndList[3])
+            if tp:
+                await message.channel.send(a+'日'+ProtCommndList[4])
+            if mm:
+                await message.channel.send(a+'日'+MarisuCommndList[0])
+            if ml:
+                await message.channel.send(a+'日'+MarisuCommndList[1])
+            if mn:
+                await message.channel.send(a+'日'+MarisuCommndList[2])
+            if mo:
+                await message.channel.send(a+'日'+MarisuCommndList[3])
+            if mp:
+                await message.channel.send(a+'日'+MarisuCommndList[4])
+            if am:
+                await message.channel.send(a+'日'+AruCommndList[0])
+            if al:
+                await message.channel.send(a+'日'+AruCommndList[1])
+            if an:
+                await message.channel.send(a+'日'+AruCommndList[2])
+            if ao:
+                await message.channel.send(a+'日'+AruCommndList[3])
+            if ap:
+                await message.channel.send(a+'日'+AruCommndList[4])
+            if rm: 
+                await message.channel.send(a+'日'+RushiCommndList[0])
+            if rl: 
+                await message.channel.send(a+'日'+RushiCommndList[1])
+            if rn: 
+                await message.channel.send(a+'日'+RushiCommndList[2])
+            if ro: 
+                await message.channel.send(a+'日'+RushiCommndList[3])
+            if rp: 
+                await message.channel.send(a+'日'+RushiCommndList[4])
+        elif a == None:
+            if tm:
+                await message.channel.send(ProtCommndList[0])
+            if tl:
+                await message.channel.send(ProtCommndList[1])
+            if tn:
+                await message.channel.send(ProtCommndList[2])
+            if to:
+                await message.channel.send(ProtCommndList[3])
+            if tp:
+                await message.channel.send(ProtCommndList[4])
+            if mm:
+                await message.channel.send(MarisuCommndList[0])
+            if ml:
+                await message.channel.send(MarisuCommndList[1])
+            if mn:
+                await message.channel.send(MarisuCommndList[2])
+            if mo:
+                await message.channel.send(MarisuCommndList[3])
+            if mp:
+                await message.channel.send(MarisuCommndList[4])
+            if am:
+                await message.channel.send(AruCommndList[0])
+            if al:
+                await message.channel.send(AruCommndList[1])
+            if an:
+                await message.channel.send(AruCommndList[2])
+            if ao:
+                await message.channel.send(AruCommndList[3])
+            if ap:
+                await message.channel.send(AruCommndList[4])
+            if rm: 
+                await message.channel.send(RushiCommndList[0])
+            if rl: 
+                await message.channel.send(RushiCommndList[1])
+            if rn: 
+                await message.channel.send(RushiCommndList[2])
+            if ro: 
+                await message.channel.send(RushiCommndList[3])
+            if rp: 
+                await message.channel.send(RushiCommndList[4])
 #ループ処理
 time_check.start()
 # Botの起動とDiscordサーバーへの接続
