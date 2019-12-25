d45c061f007e567532310f0443c367c56f3d96a0
NishiumiRyutaro <56168742+NishiumiRyutaro@users.noreply.github.com>
2019-12-25 17:20:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ async def on_message(message):
         await message.channel.send('\n'+mlist[0]+'\n'+mlist[1]+'\n'+mlist[2]+'\n'+mlist[3])
     if message.content != None:
         x = message.content
+        print(x)
         if x.find('!') == 0:
             await message.delete()  
             fromname = ('from'+' '+message.author.name)
