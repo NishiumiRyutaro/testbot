89551327ea1da9e68433b86ffb993a8c61eae925
NishiumiRyutaro <56168742+NishiumiRyutaro@users.noreply.github.com>
2020-02-14 17:21:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ async def time_check():
 async def on_message(message):
     print('[' + message.author.name + ']' + message.content)
     if message.author.bot:
-       if message.find('!') == 0:
-       await message.delete()
+        if message.find('!') == 0:
+            await message.delete()
     # 使用できるコマンド一覧
     if message.content == '!join':   
         role = discord.utils.get(message.guild.roles, name='test')
